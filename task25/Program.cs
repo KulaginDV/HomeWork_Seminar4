29f5276a4b495f2336db4ed7be3dd5b2f2b2d51a
kulaginDV <kulagin1104@yandex.ru>
2023-06-27 23:13:55 +0300

    decision task25

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.WriteLine("Input number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+
+for (int i = 1; i < b; i++)
+{
+result = result * a;
+}
+Console.WriteLine("A to the power of B is equal to: " + result);
